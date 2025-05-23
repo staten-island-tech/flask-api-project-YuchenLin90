1943236e1f5abef55d88f72ebaf12757115fc3f8
YuchenLin90 <YUchenl27@nycstudents.net>
2025-05-23 10:14:06 -0400

    commit

--- app.py
+++ app.py
@@ -57,20 +57,26 @@ def item_detail(id):
 
 
     name = item['name'].capitalize()
+    identifier = item['text_type']  
+    mc_identifier = f"minecraft:{identifier}"
 
-    mc_identifier = f"minecraft:{name.replace(' ', '_').lower()}"
-    detail_response = requests.get(f"http://yanwittmann.de/api/mcdata/itemorblock.php?identifier={mc_identifier}")
 
-    detail_data = detail_response.json()
-    describtion = detail_data.get("describtion", "No describtion avaliable.")
-    image_data = requests.get(f"http://yanwittmann.de/api/mcdata/item.php?name={name}").json()
-    image_url = image_data['items'][0]['image'] if image_data['count'] > 0 else "/static/img/placeholder.png"
+    detail_response = requests.get(f"http://yanwittmann.de/api/mcdata/itemorblock.php={mc_identifier}")
+    try:
+        detail_data = detail_response.json()
+        description = detail_data.get("description", "No description avaliable.")
+    except requests.exceptions.JSONDecodeError:
+        description = "No description availble."
+        
+        
+        image_data = requests.get(f"http://yanwittmann.de/api/mcdata/item.php?name={name}").json()
+        image_url = image_data['items'][0]['image'] if image_data['count'] > 0 else "/static/img/placeholder.png"
 
 
     return render_template("item.html", item={
            'name': name,
            'id' : id,
-           'describtion' : describtion,
+           'description' : description,
            'image' : image_url
 
         },
